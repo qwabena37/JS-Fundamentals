8e8d8d7d0a47d803cbb92de045a13978b1e30779
qwabena37 <qjaymce37@gmail.com>
2025-09-10 12:17:08 +0000

    [200~9-add.js

---
+++ 9-add.js
@@ -0,0 +1,12 @@
+function add(a, b) {
+  return a + b;
+}
+
+const arg1 = parseInt(process.argv[2], 10);
+const arg2 = parseInt(process.argv[3], 10);
+
+if (isNaN(arg1) || isNaN(arg2)) {
+  console.log("NaN");
+} else {
+  console.log(add(arg1, arg2));
+}
