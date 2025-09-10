c273de69a1b1feab6bd788981dfdc1254e470583
qwabena37 <qjaymce37@gmail.com>
2025-09-10 12:22:26 +0000

    4-concat.js

--- 4-concat.js
+++ 4-concat.js
@@ -3,7 +3,7 @@ const firstArg = process.argv[2];
 const secondArg = process.argv[3];
 
 if (firstArg === undefined || secondArg === undefined) {
-  console.log("Please provide two arguments.");
+  console.log("ABTN is undefined");
 } else {
   console.log(`${firstArg} is ${secondArg}`);
 }
