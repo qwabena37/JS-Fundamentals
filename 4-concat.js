de67053ea16bb37f65b6b10f97930b3242c0cd17
qwabena37 <qjaymce37@gmail.com>
2025-09-10 13:01:17 +0000

    4-concat.js

--- 4-concat.js
+++ 4-concat.js
@@ -1,28 +1,6 @@
 
+
 const firstArg = process.argv[2];
-const secondArg = process.argv[3];
-const thirdArg = process.argv[4]; // to detect if there's more than 2
-
-if (firstArg === undefined) {
-  console.log("No argument");
-} else if (secondArg === undefined) {
-  console.log(`one argument "${firstArg}"`);
-} else if (thirdArg === undefined) {
-  console.log(`2 arguments: "${firstArg} ${secondArg}"`);
-} else {
-  console.log("More than 2 arguments");
-}
-
-
-
-
-
-
-
-
-
-
-/*const firstArg = process.argv[2];
 const secondArg = process.argv[3];
 
 if (firstArg !== undefined && secondArg !== undefined) {
@@ -31,4 +9,4 @@ if (firstArg !== undefined && secondArg !== undefined) {
 } else {
   // Case: one or zero arguments
   console.log(`${firstArg} is ${secondArg}`);
-}*/
+}
