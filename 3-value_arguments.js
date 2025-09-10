aab677ed8fe9266ccb09fd53d2f1ea0fed39d381
qwabena37 <qjaymce37@gmail.com>
2025-09-10 13:10:48 +0000

    3-value_arguments.js

--- 3-value_arguments.js
+++ 3-value_arguments.js
@@ -1,10 +1,16 @@
 
 const firstArg = process.argv[2];
+const secondArg = process.argv[3];
+const thirdArg = process.argv[4]; // to detect if there's more than 2
 
 if (firstArg === undefined) {
   console.log("No argument");
+} else if (secondArg === undefined) {
+  console.log(`one argument "${firstArg}"`);
+} else if (thirdArg === undefined) {
+  console.log(`2 arguments: "${firstArg} ${secondArg}"`);
 } else {
-  console.log(firstArg);
+  console.log("More than 2 arguments");
 }
 
 
