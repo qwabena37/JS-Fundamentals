7490f25cd3a77869369cc1f11872f4603c7d640f
qwabena37 <qjaymce37@gmail.com>
2025-09-10 11:48:28 +0000

    add 5-to_integer.js

--- 5-to_integer.js
+++ 5-to_integer.js
@@ -4,11 +4,10 @@ const arg = process.argv[2];           // first argument
 const num = parseInt(arg, 10);         // try integer conversion
 
 if (!isNaN(num)) {
-  console.log(`Argument: ${num}`); 
-} 
-
-else {
   console.log(`My Number: ${num}`);
+  
+} else {
+  console.log("Not a number");
 }
 // Test the 5 cases
 const cases = [89, '89', -4545, '-4545', 'ALX'];
